2846cf70b473392f6df0b249258b304c9044aa9e
Qharny <cannycc1020@gmail.com>
2024-03-11 09:09:58 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   syntax/main.dart

--- syntax/main.dart
+++ syntax/main.dart
@@ -1,3 +1,24 @@
-void main(){
-  print("Hello World!");
+import 'dart:io';
+
+void main() {
+  stdout.write('Enter the first number: ');
+  int? num1 = int.tryParse(stdin.readLineSync()!);
+  stdout.write('Enter the second number: ');
+  int? num2 = int.tryParse(stdin.readLineSync()!);
+
+  if (num1 != null && num2 != null) {
+    int gcd = findGCD(num1, num2);
+    print('The greatest common divisor of $num1 and $num2 is $gcd');
+  } else {
+    print('Invalid input. Please enter valid integers.');
+  }
+}
+
+int findGCD(int a, int b) {
+  while (b != 0) {
+    int remainder = a % b;
+    a = b;
+    b = remainder;
+  }
+  return a;
 }
