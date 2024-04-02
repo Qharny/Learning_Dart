e60f22a330de78cf31186d039558889974b13b36
Qharny <cannycc1020@gmail.com>
2024-04-02 10:00:37 +0000

    modified readfile.dart

--- file/readfile.dart
+++ file/readfile.dart
@@ -1,2 +1,14 @@
 // read an external file
 
+import 'dart:io';
+
+void main() {
+  try {
+    File file =
+        File('C://Users//canny//Desktop//Learning_Dart//file//dart.txt');
+    String contents = file.readAsStringSync();
+    print(contents);
+  } catch (e) {
+    print('Error reading file: $e');
+  }
+}
