6f59f50512974eb3da86e22c8be107ab786293ef
Qharny <cannycc1020@gmail.com>
2024-08-02 22:09:47 +0000

    added new file

---
+++ project/voting.dart
@@ -0,0 +1,21 @@
+// A simple dart voting system
+
+import 'dart:io';
+
+void main() {
+  List candidate = []; // candidate list
+
+  print("Enter the name: (Enter stop break)");
+
+  // a while loop
+  while (true) {
+    String? name = stdin.readLineSync();
+    candidate.add(name);
+
+    if (name == "stop") {
+      break;
+    }
+    candidate.add(name);
+  }
+  print(candidate);
+}
